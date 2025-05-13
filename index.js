5ca69189fb0fd2d39b2b9b610b9b3a2b17afc5e7
Juan Silva <juanse909@hotmail.com>
2025-05-13 15:15:25 -0500

    feat: update index to add new routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { createServer } = require("http");
 
 const usersRouter = require("./server/routes/users.router");
 const screen1EventsRouter = require("./server/routes/screen1Events.router");
+const dbRouter = require("./server/routes/db.router"); // Add our new router
 const { initSocketInstance } = require("./server/services/socket.service");
 
 const PORT = 5050;
@@ -15,13 +16,20 @@ const httpServer = createServer(app);
 app.use(express.json());
 app.use("/app1", express.static(path.join(__dirname, "app1")));
 app.use("/app2", express.static(path.join(__dirname, "app2")));
+app.use("/app3", express.static(path.join(__dirname, "app3"))); // Add our new app
 
 // Routes
 app.use("/", usersRouter);
 app.use("/", screen1EventsRouter);
+app.use("/api", dbRouter); // Add our new routes under /api prefix
 
 // Services
 initSocketInstance(httpServer);
+
+// Main route
+app.get("/", (req, res) => {
+  res.redirect("/app3"); // Redirect to our new app
+});
 
 httpServer.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
